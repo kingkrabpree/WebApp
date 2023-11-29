7824ea9690ab03e8dbe6bbfee3cd623ab817880f
kingkrabpree <150315923+kingkrabpree@users.noreply.github.com>
2023-11-29 15:04:57 +0700

    week4

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { NavComponent } from './nav/nav.component'
+import { FormsModule } from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
